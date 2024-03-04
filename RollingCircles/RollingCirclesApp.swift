f829a626380a03d5d2d81eae935f1ae9b94b977e
Abhijit Joshi <abhijit.joshi@parkmobile.io>
2024-03-03 20:59:15 -0500

    Initial Commit

---
+++ RollingCircles/RollingCirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollingCirclesApp.swift
+//  RollingCircles
+//
+//  Created by Abhijit Joshi on 3/3/24.
+//
+
+import SwiftUI
+
+@main
+struct RollingCirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
